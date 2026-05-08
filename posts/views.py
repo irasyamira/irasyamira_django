cf9cb9f98bb744f0ecc779c2c411861438d15949
irasyamira_pythany <irasyamira.min@gmail.com>
2026-05-08 13:58:21 +0000

    Added html template files for polls. Pending for posts app

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, posts. First app setup complete!")
+    latest_posts_list = Post.objects.order_by("-pub_date")[:5]
+    output = ", ".join([p.title for p in latest_posts_list])
+    return HttpResponse(output)
+    # return HttpResponse("Hello, posts. First app setup complete!")
