9abb79b6ce23a1f6660bbc44214cbe16634f69ce
irasyamira_pythany <irasyamira.min@gmail.com>
2026-05-09 14:14:08 +0000

    Further implementation of html templates for posts

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,19 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+
 from .models import Post
 # Create your views here.
-from django.http import HttpResponse
 
 def index(request):
     latest_posts_list = Post.objects.order_by("-pub_date")[:5]
     context = {"latest_posts_list": latest_posts_list}
     return render(request, "posts/index.html", context)
-    # return HttpResponse("Hello, posts. First app setup complete!")
+
+def detail(request, post_id):
+    try:
+        obj = Post.objects.get(pk=post_id)
+        title = obj.title
+        content = obj.content
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+    return render(request, "posts/detail.html", {"title": title,"content":content})
