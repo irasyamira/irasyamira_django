ec298dbd87734734f8bd503396a34456691cf5d1
irasyamira_pythany <irasyamira.min@gmail.com>
2026-05-09 07:35:41 +0000

    Updated posts to use html template

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     latest_posts_list = Post.objects.order_by("-pub_date")[:5]
-    output = ", ".join([p.title for p in latest_posts_list])
-    return HttpResponse(output)
+    context = {"latest_posts_list": latest_posts_list}
+    return render(request, "posts/index.html", context)
     # return HttpResponse("Hello, posts. First app setup complete!")
